8b7ff4789f24ebac21025cb217cc59a98eff85cd
Shino <anonymous.from.applecity@gmail.com>
2017-02-22 00:30:44 +0600

    Implemented BinarySearchTree.add
    Implemented the add method of the class and written some dummy tests to it.

---
+++ src/custom/binary_tree_tests/Test.java
@@ -0,0 +1,22 @@
+package custom.binary_tree_tests;
+
+import custom.trees.BinaryTree;
+
+public class Test {
+
+    public static void main(String[] args)
+    {
+        BinaryTree<Integer> root = new BinaryTree<>(0);
+
+        root.setLeftChild(new BinaryTree<>(1));
+        root.setRightChild(new BinaryTree<>(2));
+
+        root.getLeftChild().setLeftChild(new BinaryTree<>(3));
+        root.getLeftChild().setRightChild(new BinaryTree<>(4));
+
+        root.getRightChild().setLeftChild(new BinaryTree<>(5));
+        root.getRightChild().setRightChild(new BinaryTree<>(6));
+
+        System.out.println(root.toString());
+    }
+}
